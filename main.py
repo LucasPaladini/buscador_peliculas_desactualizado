14b4722209fad5269cbbbc5a78a3daf8fbf588fd
LucasPaladini <lucas.paladini@alumnos.ispi4038.edu.ar>
2024-10-17 00:57:16 -0300

    Creo modulo

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 from PySide6.QtWidgets import QApplication
-from vista import VentanaPrincipal
+from busqueda_peliculas import VentanaPrincipal
 import sys
-
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
     ventana_principal = VentanaPrincipal()
     ventana_principal.show()
-
     sys.exit(app.exec())
-
-

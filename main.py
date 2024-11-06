dbfb964c0b34fac4cdf0ef48cb73341cb41231a1
LucasPaladini <lucas.paladini@alumnos.ispi4038.edu.ar>
2024-11-06 00:49:55 -0300

    Ahora main ejecuta un método del controlador

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from PySide6.QtWidgets import QApplication
-from busqueda_peliculas import VentanaPrincipal
 import sys
+
+from controlador import Controlador
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
-    ventana_principal = VentanaPrincipal()
-    ventana_principal.show()
+    controlador = Controlador()
+    controlador.ejecutar()
     sys.exit(app.exec())
